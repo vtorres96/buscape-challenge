cdf911a655a874f1e83f444a2d7e7a2089f6240a
vtorres96 <victorcdc96@gmail.com>
2019-06-29 04:15:20 -0300

    creating header component

---
+++ app/src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+import { HeaderStyle } from './HeaderStyle'
+import ButtonMenu from '../ButtonMenu/ButtonMenu'
+
+import logo from '../../assets/img/logo-buscape.png'
+
+export default function Header() {
+  return (
+    <HeaderStyle>
+        <div className="header-content">
+            <Link to="/">
+              <img src={logo} alt="Buscapé" className="logo"/>
+            </Link>
+            <ButtonMenu
+                onClick=""
+            />
+        </div>
+    </HeaderStyle>
+  );
+}

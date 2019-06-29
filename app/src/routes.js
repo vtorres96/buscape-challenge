419f349e89b767655a02af6a4a729bee9922a293
vtorres96 <victorcdc96@gmail.com>
2019-06-29 04:16:41 -0300

    creating route file

---
+++ app/src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+function Routes(){
+    return(
+        <Switch>
+            <Route path="/" exact/>
+        </Switch>
+    )
+}
+
+export default Routes
